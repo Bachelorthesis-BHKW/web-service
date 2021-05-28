a678a466346d54ed3b5d59625582bff64c50770a
Janik  Steegmüller <janik.steegmueller@student.uni-tuebingen.de>
2021-05-28 13:07:55 +0200

    feat: add request for schedule now

--- src/services/ESScheduleService.ts
+++ src/services/ESScheduleService.ts
@@ -8,3 +8,18 @@ export async function getESScheduleByEnergySystemId(
   if (!esSchedule) throw new ExpressError(ErrorCode.NOT_FOUND_404);
   return esSchedule;
 }
+
+export async function getESScheduleNowByEnergySystemId(
+  energySystemId: number
+): Promise<number> {
+  const esSchedule = await ESSchedule.findOne({ where: { energySystemId } });
+  if (!esSchedule) throw new ExpressError(ErrorCode.NOT_FOUND_404);
+  const timeArray = JSON.parse(esSchedule.schedule) as number[];
+  const date = new Date();
+  const timeInMinutes = date.getMinutes() + date.getHours() * 60;
+  const index = Math.floor(timeInMinutes / esSchedule.timeIntervalMin);
+  if (!index || index < 0 || index >= timeArray.length)
+    throw new ExpressError(ErrorCode.INTERNAL_SERVER_ERROR_500);
+
+  return timeArray[index];
+}
