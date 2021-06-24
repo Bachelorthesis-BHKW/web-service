0c10e1387dd81e4a2f584c8fade428b32a8e4262
Janik  Steegmüller <janik.steegmueller@student.uni-tuebingen.de>
2021-06-24 11:01:27 +0200

    refactor: switch from id to real object for better dep.injection

--- src/subscriber/NewEnergySystem.ts
+++ src/subscriber/NewEnergySystem.ts
@@ -2,11 +2,12 @@ import MainEventEmitter, { Events } from "./MainEventEmitter";
 import { addRegionToEnergySystem } from "../services/NominatimService";
 import { createNewCircularBufferPointer } from "../services/CircularBufferPointerService";
 import { fetchHourlyWeatherForEnergySystem } from "../services/WeatherForecastService";
+import { EnergySystem } from "../models/EnergySystem";
 
 export function subscribeToNewEnergySystem(emitter: MainEventEmitter): void {
-  emitter.on(Events.NEW_ENERGY_SYSTEM, async (energySystemId: number) => {
-    await createNewCircularBufferPointer(energySystemId);
-    await fetchHourlyWeatherForEnergySystem(energySystemId);
-    await addRegionToEnergySystem(energySystemId);
+  emitter.on(Events.NEW_ENERGY_SYSTEM, async (energySystem: EnergySystem) => {
+    await createNewCircularBufferPointer(energySystem);
+    await fetchHourlyWeatherForEnergySystem(energySystem);
+    await addRegionToEnergySystem(energySystem);
   });
 }
