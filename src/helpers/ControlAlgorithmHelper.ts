3d33357faaeaa97cf93319aca12738c67899c74e
Janik  Steegmüller <janik.steegmueller@student.uni-tuebingen.de>
2021-06-24 11:00:20 +0200

    feat: call control algorithm with holiday bool

--- src/helpers/ControlAlgorithmHelper.ts
+++ src/helpers/ControlAlgorithmHelper.ts
@@ -1,13 +1,20 @@
 import { PythonShell } from "python-shell";
 import config from "../config";
+import isWeekendOrHoliday from "./isWeekendOrHoliday";
+import { EnergySystem } from "../models/EnergySystem";
 
 export default class ControlAlgorithmHelper {
-  static runWithEnergySystemId(energySystemId: number): void {
+  static runWithEnergySystem(energySystem: EnergySystem): void {
     if (!config.controlAlgorithmPath)
       throw Error("Control Algorithm path not set!");
     PythonShell.run(
       config.controlAlgorithmPath,
-      { args: [energySystemId.toString(10)] },
+      {
+        args: [
+          energySystem.energySystemId.toString(10),
+          String(isWeekendOrHoliday(new Date(), energySystem.region)),
+        ],
+      },
       (err, result) => {
         if (err) console.log(err);
         else console.log(result);
