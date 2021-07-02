cc630ff7ccdc1afe349a05e8f32f8fc134e7bdd1
Janik  Steegmüller <janik.steegmueller@student.uni-tuebingen.de>
2021-07-02 14:57:28 +0200

    refactor: run algo event based

--- src/subscriber/MainEventEmitter.ts
+++ src/subscriber/MainEventEmitter.ts
@@ -1,6 +1,7 @@
 import EventEmitter from "events";
 import { subscribeToNewEnergySystem } from "./NewEnergySystem";
 import { EnergySystem } from "../models/EnergySystem";
+import { subscribeToRunAlgorithm } from "./RunAlgorithm";
 
 export default class MainEventEmitter extends EventEmitter {
   private static instance: MainEventEmitter;
@@ -13,6 +14,7 @@ export default class MainEventEmitter extends EventEmitter {
     if (!MainEventEmitter.instance) {
       MainEventEmitter.instance = new MainEventEmitter();
       subscribeToNewEnergySystem(MainEventEmitter.instance);
+      subscribeToRunAlgorithm(MainEventEmitter.instance);
     }
     return MainEventEmitter.instance;
   }
@@ -20,8 +22,13 @@ export default class MainEventEmitter extends EventEmitter {
   public newEnergySystem(energySystem: EnergySystem): void {
     this.emit(Events.NEW_ENERGY_SYSTEM, energySystem);
   }
+
+  public runAlgorithm(energySystem: EnergySystem): void {
+    this.emit(Events.RUN_ALGORITHM, energySystem);
+  }
 }
 
 export enum Events {
   NEW_ENERGY_SYSTEM = "new_es",
+  RUN_ALGORITHM = "run_algo",
 }
