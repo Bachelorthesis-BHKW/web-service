fce39ab508fdca1d30afba40051a6c9ccf927cd0
Janik  Steegmüller <janik.steegmueller@student.uni-tuebingen.de>
2021-07-02 14:45:07 +0200

    feat: add cron based algorithm calculation

--- src/subscriber/NewEnergySystem.ts
+++ src/subscriber/NewEnergySystem.ts
@@ -3,11 +3,13 @@ import { addRegionToEnergySystem } from "../services/NominatimService";
 import { createNewCircularBufferPointer } from "../services/CircularBufferPointerService";
 import { fetchHourlyWeatherForEnergySystem } from "../services/WeatherForecastService";
 import { EnergySystem } from "../models/EnergySystem";
+import { setTimingForEnergySystem } from "../services/AlgorithmTimingService";
 
 export function subscribeToNewEnergySystem(emitter: MainEventEmitter): void {
   emitter.on(Events.NEW_ENERGY_SYSTEM, async (energySystem: EnergySystem) => {
     await createNewCircularBufferPointer(energySystem);
     await fetchHourlyWeatherForEnergySystem(energySystem);
     await addRegionToEnergySystem(energySystem);
+    await setTimingForEnergySystem(energySystem);
   });
 }
