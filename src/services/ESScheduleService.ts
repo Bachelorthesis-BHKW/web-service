6f0e76a8726a78e1cd7021b4cbe5d3e866472a86
Fabi22-10 <93785247+Fabi22-10@users.noreply.github.com>
2023-03-25 12:40:59 +0100

    Umstellung Sommerzeit

--- src/services/ESScheduleService.ts
+++ src/services/ESScheduleService.ts
@@ -15,7 +15,7 @@ export async function getESScheduleByEnergySystemId(
   const oneDayAgo = new Date(new Date().getTime() - 1000 * 60 * 60 * 24);
   if (!esSchedule || esSchedule.updatedAt < oneDayAgo)
     throw new ExpressError(ErrorCode.NOT_FOUND_404);
-  const timeCorrection = 1; // TODO: Zeitzone händisch anpassen
+  const timeCorrection = 2; // TODO: Zeitzone händisch anpassen
                             // mitteleuropäische Winterzeit: timeCorrection = 1
                             // mitteleuropäische Sommerzeit: timeCorrection = 2
   const timeInTimeZone = new Date(esSchedule.updatedAt.getTime() + 1000 * 60 * 60 * timeCorrection) ;
