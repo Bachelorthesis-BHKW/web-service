4105bae4c82598d74d4d35165f8f7c51856d1720
Fabi22-10 <you@example.com>
2024-09-28 16:26:29 +0200

    fix: assign pv power to forecast hour

--- src/services/WeatherForecastService.ts
+++ src/services/WeatherForecastService.ts
@@ -9,6 +9,7 @@ import { EnergySystem } from "../models/EnergySystem";
 import { ESComponentType } from "../es_components/ESComponentType";
 import PV from "../es_components/PV";
 import ForecastSolarResponse from "../apis/ForecastSolar/ForecastSolarResponse";
+import { time } from "cron";
 
 const openWeatherClient = OpenWeatherMap.getInstance();
 const forecastSolarClient = ForecastSolar.getInstance();
@@ -72,7 +73,7 @@ export async function fetchHourlyWeatherForEnergySystem(
     // Match PV-Data to Weather-Forecast time
     let absPVPower = 0;
     for (const hourlyPVForecast of hourlyPVForecasts) {
-      absPVPower += getPVPowerForDate(hourlyPVForecast, date);
+      absPVPower += getPVPowerForDate(hourlyPVForecast, date, energySystem);
     }
 
     await createWeatherForecast(
@@ -84,31 +85,103 @@ export async function fetchHourlyWeatherForEnergySystem(
 
 function getPVPowerForDate(
   hourlyPVForecast: ForecastSolarResponse,
-  date: Date
+  date: Date,
+  energySystem: EnergySystem
 ) {
-  let maxPVPower = 0;
+  const desiredStartDate = date;
+  const desiredEndDate = new Date(date.getTime() + 1000 * 60 * 60); // date + 1 hour
   const pvDates = Object.keys(hourlyPVForecast.result.watt_hours_period);
-  pvDates.forEach((pvDate) => {
-    const [year, month, day, hour, minute, second] = pvDate.split(/[- :]/);
-    const pvDateObject = new Date(
-      parseInt(year),
-      parseInt(month) - 1, // Month in JavaScript Date object is zero-based
-      parseInt(day),
-      parseInt(hour),
-      parseInt(minute),
-      parseInt(second)
-    );
+  let timeZone = 1;
+  if (energySystem.sommerzeit) {
+    timeZone = 2;
+  }
 
-    const differenceInMinutes = Math.floor(
-      (date.getTime() - pvDateObject.getTime()) / 1000 / 60
-    );
-    //console.log("difference in minutes:", differenceInMinutes);
-    if (differenceInMinutes < 60 && differenceInMinutes >= 0) {
-      const pvPower = hourlyPVForecast.result.watt_hours_period[pvDate];
-      if (pvPower > maxPVPower) {
-        maxPVPower = pvPower;
-      }
+  let pvPower = 0;
+
+  // Collect all PV-intervals that are relevant for the desired hour
+  for (let i = 1; i < pvDates.length; i++) {
+    const pvDate = pvDates[i];
+    const prevPvDate = pvDates[i - 1];
+    const pvIntervalStartDate = stringToDate(prevPvDate, timeZone);
+    const pvIntervalEndDate = stringToDate(pvDate, timeZone);
+    let relevantIntervalStartDate = new Date(desiredStartDate);
+    let relevantIntervalEndDate = new Date(desiredEndDate);
+    let intervalIsRelevant = false;
+
+    if (
+      desiredStartDate <= pvIntervalStartDate &&
+      pvIntervalStartDate < desiredEndDate
+    ) {
+      // the interval starts in the desired hour
+      intervalIsRelevant = true;
+      relevantIntervalStartDate = pvIntervalStartDate;
     }
-  });
-  return maxPVPower;
+
+    if (
+      desiredStartDate < pvIntervalEndDate &&
+      pvIntervalEndDate <= desiredEndDate
+    ) {
+      // the interval ends in the desired hour
+      intervalIsRelevant = true;
+      relevantIntervalEndDate = pvIntervalEndDate;
+    }
+
+    if (
+      pvIntervalStartDate < desiredStartDate &&
+      desiredEndDate < pvIntervalEndDate
+    ) {
+      // the interval starts before and ends after the desired hour
+      intervalIsRelevant = true;
+      relevantIntervalStartDate = desiredStartDate;
+      relevantIntervalEndDate = desiredEndDate;
+    }
+
+    if (!intervalIsRelevant) {
+      continue;
+    }
+    const relevantMinutesOfPvInterval = Math.round(
+      (relevantIntervalEndDate.getTime() -
+        relevantIntervalStartDate.getTime()) /
+        1000 /
+        60
+    );
+    const totalMinutesOfPvInterval = Math.round(
+      (pvIntervalEndDate.getTime() - pvIntervalStartDate.getTime()) / 1000 / 60
+    );
+    const totalPowerOfPvInterval =
+      hourlyPVForecast.result.watt_hours_period[pvDate];
+    const relevantPowerOfPvInterval = Math.round(
+      (totalPowerOfPvInterval * relevantMinutesOfPvInterval) /
+        totalMinutesOfPvInterval
+    );
+    if (relevantMinutesOfPvInterval != 60 || totalMinutesOfPvInterval != 60) {
+      console.log(
+        desiredStartDate.toTimeString() + ":",
+        "Interpolated " +
+          relevantPowerOfPvInterval +
+          " Wh in " +
+          relevantMinutesOfPvInterval +
+          " minutes."
+      );
+    }
+    pvPower += relevantPowerOfPvInterval;
+  }
+  console.log(
+    desiredStartDate.toTimeString() + ":",
+    pvPower + " Wh for PV system."
+  );
+  return pvPower;
+}
+
+function stringToDate(dateString: string, timeZone: number) {
+  const [year, month, day, hour, minute, second] = dateString.split(/[- :]/);
+  const dateObject = new Date(
+    parseInt(year),
+    parseInt(month) - 1, // Month in JavaScript Date object is zero-based
+    parseInt(day),
+    parseInt(hour) - timeZone,
+    parseInt(minute),
+    parseInt(second)
+  );
+  return dateObject;
 }
